dbeaaced3d5d723179162a4c07d5f099ad8b8a65
SidhanthReddy <22211a67a1@bvrit.ac.in>
2024-11-30 23:56:04 +0530

    Basic Home Page Layout

---
+++ library/php/home.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/home.css">
+    <link rel="stylesheet" href="./css/carousel.css">
+</head>
+<body>
+    <div class="main">
+        <div class="header">
+            <div class="content">
+                <h1 class="h1">Book</h1>
+                <h1 class="h2">Quest</h1>
+            </div>
+        </div>
+        <div class="search">
+            <input type="text" class = "search_input" placeholder="Find by Book, Author, Ref ID">
+            <button class = "search_button">
+                <img src="./images/search_ic.png" alt="search_b" class="search_icon">
+            </button>
+        </div>
+    </div>
+    <h3 class="popular_heading">Most Liked Books 💓:</h3>
+    <ul class="gallery">
+	<li style="background: #f6bd60;"></li>
+	<li style="background: #f7ede2;"></li>
+	<li style="background: #f5cac3;"></li>
+	<li style="background: #84a59d;"></li>
+	<li style="background: #f28482;"></li>
+</ul>
+</body>
+<script src="carousel.js"></script>
+</html>
